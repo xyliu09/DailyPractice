311f10358f0e9a4547f7b2338c75f03a715abeb2
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-07-30 00:27:00 -0400

    '50'

---
+++ LeetcodeSolutions/Math/50_Power_x_n.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n < 0 :
+            x = 1/x
+            n = -n
+        power = 1
+        while n:
+            if n & 1:
+                power *= x
+            n >>= 1
+            x *= x
+        return power
+    """     
+    class Solution:
+    def myPow(self, x, n):
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        return self.myPow(x*x, n/2)
+    """      

af6fec03a01cf1e73e67c41301c9b6618363c930
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-08-10 22:42:11 -0400

    'all'

---
+++ LeetcodeSolutions/Dfs/39_Combination_Sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(candidates, target, 0, [], res)
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return
+        if target == 0:
+            res.append(path)
+            return
+        for i in range(index, len(nums)):
+            self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
---
+++ LeetcodeSolutions/Two_Pointers/141_Linked_List_Cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head: return False
+        slow, fast = head, head.next
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow, fast = slow.next, fast.next.next
+        return False

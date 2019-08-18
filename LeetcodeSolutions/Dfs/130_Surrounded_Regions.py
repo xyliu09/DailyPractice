1a94f26e404cfdfc6cd14dbe9347719ea1c6975e
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-08-18 12:17:37 -0400

    '130'

---
+++ LeetcodeSolutions/Dfs/130_Surrounded_Regions.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+        for i in range(len(board)):
+            if board[i][0] == "O":
+                self.dfs(board, i, 0)
+            if board[i][-1] == "O":
+                self.dfs(board, i, len(board[0]) - 1)
+        for j in range(len(board[0])):
+            if board[0][j] == "O":
+                self.dfs(board, 0, j)
+            if board[-1][j] == "O":
+                self.dfs(board, len(board) - 1, j)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                if board[i][j] == "*":
+                    board[i][j] = "O"
+
+    def dfs(self, board, i, j):
+        if i < 0 or j < 0 or i > len(board) - 1 or j > len(board[0]) - 1 or board[i][j] != "O":
+            return
+        board[i][j] = "*"
+        for x, y in Solution.directions:
+            self.dfs(board, i + x, j + y)

ce41da5578d016fb3317a5f9f7a39fd142dbeb83
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-08-18 10:39:02 -0400

    'bfs'

--- LeetcodeSolutions/Bfs/695_Max_Area_of_Island.py
+++ LeetcodeSolutions/Bfs/695_Max_Area_of_Island.py
@@ -1,27 +1,31 @@
 class Solution(object):
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    maxArea = 0
 
-    def numIslands(self, grid):
+    def maxAreaOfIsland(self, grid):
         """
-        :type grid: List[List[str]]
+        :type grid: List[List[int]]
         :rtype: int
         """
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+                if grid[i][j] == 1:
+                    Solution.maxArea = 0
                     self.bfs(grid, i, j)
-                    count += 1
+                    count = max(count, Solution.maxArea)
         return count
 
     def bfs(self, grid, i, j):
         q = []
         q.append((i, j))
+        grid[i][j] = 0
         while q:
             m, n = q.pop(0)
+            Solution.maxArea += 1
             for x, y in Solution.directions:
                 if m + x < 0 or n + y < 0 or m + x > len(grid) - 1 or n + y > len(grid[0]) - 1 or grid[m + x][
-                            n + y] != "1":
+                            n + y] != 1:
                     continue
-                grid[m + x][n + y] = "*"
+                grid[m + x][n + y] = 0
                 q.append((m + x, n + y))

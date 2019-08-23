4b95b02577f12fd0d531495aa317c5e522f1639c
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-08-22 23:42:15 -0400

    '9'

---
+++ LeetcodeSolutions/Math/9_Palindrome_Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        revertNumber = 0
+        while x > revertNumber:
+            revertNumber = revertNumber * 10 + x % 10
+            x /= 10
+        return x == revertNumber or x == revertNumber / 10

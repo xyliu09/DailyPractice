09477a8612eda125b28d63c48258079935dff6ab
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-08-29 00:22:06 -0400

    '160'

---
+++ LeetcodeSolutions/Linkedlist/160_Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA == None or headB == None:
+            return None
+
+        A_pointer = headA
+        B_pointer = headB
+
+        while A_pointer != B_pointer:
+            A_pointer = headB if A_pointer == None else A_pointer.next
+            B_pointer = headA if B_pointer == None else B_pointer.next
+
+        return A_pointer

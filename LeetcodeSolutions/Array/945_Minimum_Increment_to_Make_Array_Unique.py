86da55f7981a946bdc819eeae676128295a33971
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-09-15 21:52:40 -0400

    '945_Minimum_Increment_to_Make_Array_Unique.py

---
+++ LeetcodeSolutions/Array/945_Minimum_Increment_to_Make_Array_Unique.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minIncrementForUnique(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        if not A or len(A) <= 1:
+            return 0
+        A.sort()
+        res, prev = 0, A[0]
+        for i in range(1,len(A)):
+            expect = prev+1
+            if A[i] > expect:
+                res += 0
+            else:
+                res += expect -A[i]
+            prev = max(A[i], expect)
+        return res

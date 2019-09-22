2f44b05d2388191d94ab7d6e8dc5f13e0a9e3b26
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-09-21 22:43:56 -0400

    '17'

---
+++ LeetcodeSolutions/Backtracking/17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r','s'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w','x', 'y','z']}
+        def backtracking(combination, next_digits):
+            if len(next_digits) == 0:
+                output.append(combination)
+            else:
+                for letter in phone[next_digits[0]]:
+                    backtracking(combination+letter, next_digits[1:])
+        output = []
+        if digits:
+            backtracking("", digits)
+        return output

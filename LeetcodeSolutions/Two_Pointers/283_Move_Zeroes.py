927ef20cde8c62a92f9351cdbb340326cac67551
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-03 23:59:41 -0400

    283

--- LeetcodeSolutions/Two_Pointers/283_Move_Zeroes.py
+++ LeetcodeSolutions/Two_Pointers/283_Move_Zeroes.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        zero = 0
+        nz = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1
+                nums[i], nums[nz] = nums[zero], nums[i]
+                nz += 1

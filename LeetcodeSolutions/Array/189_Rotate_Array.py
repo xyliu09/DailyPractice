e4631321560203747d1cd108aae0565e7968d664
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-05 14:34:10 -0400

    189

--- LeetcodeSolutions/Array/189_Rotate_Array.py
+++ LeetcodeSolutions/Array/189_Rotate_Array.py
@@ -9,11 +9,9 @@ class Solution(object):
         self.reverse(nums, 0, len(nums) - 1)
         self.reverse(nums, 0, k - 1)
         self.reverse(nums, k, len(nums) - 1)
-        return nums
 
     def reverse(self, nums, start, end):
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
-

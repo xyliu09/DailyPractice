83f5d9b75b518e9cc2627ebecb9c78a84a80f020
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-06 12:43:06 -0400

    152

---
+++ LeetcodeSolutions/Array/152_Maximum_Product_Subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res, imin, imax = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            candidate = (nums[i], nums[i]*imin, nums[i]*imax)
+            imin = min(candidate)
+            imax = max(candidate)
+            res = max(res, imax)
+        return res

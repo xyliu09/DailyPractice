c3ff6549e129b34df36f01b8f13dc57de7a70ca9
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-13 15:34:51 -0400

    7_Reverse_Integer.py

--- LeetcodeSolutions/Math/7_Reverse_Integer.py
+++ LeetcodeSolutions/Math/7_Reverse_Integer.py
@@ -4,14 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x >= 2**31-1 or x <= -2**31: return 0
-        else:
-            strg = str(x)
-            if x >= 0 :
-                revst = strg[::-1]
-            else:
-                temp = strg[1:]
-                temp2 = temp[::-1]
-                revst = "-" + temp2
-        if int(revst) >= 2**31-1 or int(revst) <= -2**31: return 0
-        else: return int(revst)
+        isNegative = -1 if x <0 else 1
+        value = abs(x)
+        new = 0
+        while value >0:
+            new = new*10 + value%10
+            if new > 2**31-1 or new < -2**31:
+                return 0
+            value /=10
+        return isNegative*new

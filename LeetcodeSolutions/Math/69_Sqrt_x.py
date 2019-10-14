35d415f28606c5f89e431b4fe172d38a6b2cf424
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-13 20:18:57 -0400

    69

--- LeetcodeSolutions/Math/69_Sqrt_x.py
+++ LeetcodeSolutions/Math/69_Sqrt_x.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        l, r = 0, x
-        while l + 1 < r:
-            mid = (l + r) // 2
-            if mid * mid <= x < (mid + 1) * (mid + 1):
-                return mid
-            elif mid * mid > x:
+        l, r = 1, x
+        while l+1<r:
+            mid = (l+r)//2
+            if mid *mid >x:
                 r = mid
             else:
                 l = mid
-        return x
+        if r *r > x:
+            return l
+        else:
+            return r

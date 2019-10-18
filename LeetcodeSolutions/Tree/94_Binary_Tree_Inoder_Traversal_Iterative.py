1aa882f7edcf6e93b714b6b0ed6ae5ff8f8e3c30
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-18 13:09:09 -0400

    94

---
+++ LeetcodeSolutions/Tree/94_Binary_Tree_Inoder_Traversal_Iterative.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        curr, stack, res = root, [], []
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res

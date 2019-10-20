e2827da89f67cb624a760341e2236c8539c12f16
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-19 22:01:26 -0400

    230_Kth_Smallest_Element_in_a_BST_Iterative.py

---
+++ LeetcodeSolutions/Tree/230_Kth_Smallest_Element_in_a_BST_Iterative.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right

d3de8cccc4801805b98df5ff0d8dea382e1cb0d2
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-21 19:43:55 -0400

    145

---
+++ LeetcodeSolutions/Tree/145_Binary_Tree_Postorder_Traversal_Iterative_Two_Stack.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+
+        stack1, stack2, res = [root], [], []
+        while stack1:
+            curr = stack1.pop()
+            stack2.append(curr.val)
+            if curr.left:
+                stack1.append(curr.left)
+            if curr.right:
+                stack1.append(curr.right)
+        while stack2:
+            res.append(stack2.pop())
+        return res

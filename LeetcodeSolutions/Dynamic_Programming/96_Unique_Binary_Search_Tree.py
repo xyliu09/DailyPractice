a743836533c7521d81433c6ed9882fd32ba88e06
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-22 20:41:03 -0400

    96_Unique_Binary_Search_Tree.py

---
+++ LeetcodeSolutions/Dynamic_Programming/96_Unique_Binary_Search_Tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for _ in range(0, n+1)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            for j in range(1,i+1):
+                dp[i] += dp[j-1] *dp[i-j]
+        return dp[-1]

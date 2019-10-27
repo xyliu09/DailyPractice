ccc665cc86f27c05ed93d76c7a4bdfddf213375b
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-10-27 15:28:34 -0400

    110

---
+++ LeetcodeSolutions/Tree/110_Balanced_Binary_Tree_Recursive_Two_Pass.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        left = self.height(root.left)
+        right = self.height(root.right)
+        return abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def height(self, root):
+        if not root:
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
+

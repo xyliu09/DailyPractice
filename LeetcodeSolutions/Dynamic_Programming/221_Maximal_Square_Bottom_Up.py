c7ae85014788088dce8c786e14a68771986bada5
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-11-02 16:19:49 -0400

    221

---
+++ LeetcodeSolutions/Dynamic_Programming/221_Maximal_Square_Bottom_Up.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]: return 0
+        dp = [[0 for j in range(len(matrix[0]) + 1)] for i in range(len(matrix) + 1)]
+        maxlen = 0
+        for i in range(1, len(matrix) + 1):
+            for j in range(1, len(matrix[0]) + 1):
+                if matrix[i - 1][j - 1] == '1':
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
+                    maxlen = max(maxlen, dp[i][j])
+        return maxlen ** 2
+

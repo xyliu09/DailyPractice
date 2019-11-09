414cff1bd91f45370e4c04d8993d284bbe8472ae
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-11-08 22:06:17 -0500

    120

---
+++ LeetcodeSolutions/Dynamic_Programming/120_Triangle_Bottom_Up.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        dp = [[2 ** 31 for _ in triangle[-1]] for _ in triangle]
+        dp[0][0] = triangle[0][0]
+        for i in range(1, len(triangle)):
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            dp[i][-1] = dp[i - 1][-1] + triangle[i - 1][-1]
+            for j in range(1, len(triangle[i])):
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+
+        return min(dp[-1])

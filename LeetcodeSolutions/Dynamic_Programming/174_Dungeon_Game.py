82dd02daaf9bb42b409c13f963c71962a6435285
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-11-11 12:46:36 -0500

    174

---
+++ LeetcodeSolutions/Dynamic_Programming/174_Dungeon_Game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[2 ** 31 for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[m][n - 1], dp[m - 1][n] = 1, 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j])
+        return dp[0][0]

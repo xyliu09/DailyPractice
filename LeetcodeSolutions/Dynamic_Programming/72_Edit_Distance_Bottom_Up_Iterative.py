2f5dea2ee55f998ff4d874c16059cc2e651e4778
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-11-11 19:31:58 -0500

    72

---
+++ LeetcodeSolutions/Dynamic_Programming/72_Edit_Distance_Bottom_Up_Iterative.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
+        dp[0] = [j for j in range(len(word2) + 1)]
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                dp[i][j] = dp[i - 1][j - 1] if word1[i - 1] == word2[j - 1] else min(dp[i - 1][j], dp[i][j - 1],
+                                                                                     dp[i - 1][j - 1]) + 1
+
+        return dp[-1][-1]

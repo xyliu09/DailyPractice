40bb204844f33df2aadddc1ac5eaaf73760ae113
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-11-18 23:39:08 -0500

    207

--- LeetcodeSolutions/Dfs/207_Course_Schedule.py
+++ LeetcodeSolutions/Dfs/207_Course_Schedule.py
@@ -5,26 +5,23 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-
         graph = [[] for _ in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
-
         for x, y in prerequisites:
             graph[x].append(y)
-
-        def dfs(i):
-            if visited[i] == -1:
-                return False
-            if visited[i] == 1:
-                return True
-            visited[i] = -1
-            for node in graph[i]:
-                if not dfs(node):
-                    return False
-            visited[i] = 1
-            return True
-
         for i in range(numCourses):
-            if not dfs(i):
+            if not self.dfs(graph, i, visited):
                 return False
+        return True
+
+    def dfs(self, graph, i, visited):
+        if visited[i] == -1:
+            return False
+        if visited[i] == 1:
+            return True
+        visited[i] = -1
+        for c in graph[i]:
+            if not self.dfs(graph, c, visited):
+                return False
+        visited[i] = 1
         return True

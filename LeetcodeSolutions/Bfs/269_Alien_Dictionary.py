234e40c813b9299837f7a2b02721d4c51f9ff2d0
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-12-04 07:58:38 -0500

    269_Alien_Dictionary.py

---
+++ LeetcodeSolutions/Bfs/269_Alien_Dictionary.py
@@ -0,0 +1,39 @@
+from collections import deque, defaultdict
+
+
+class Solution(object):
+    def alienOrder(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        graph = defaultdict(set)
+        indegree = {}
+        for i in range(len(words)):
+            for j in range(len(words[i])):
+                graph[words[i][j]] = set()
+                indegree[words[i][j]] = 0
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            for j in range(min(len(w1), len(w2))):
+                if w1[j] != w2[j]:
+                    if w2[j] not in graph[w1[j]]:
+                        graph[w1[j]].add(w2[j])
+                        indegree[w2[j]] = indegree.get(w2[j]) + 1
+                    break
+        queue = deque([])
+        for c in indegree:
+            if indegree[c] == 0:
+                queue.append(c)
+        res = []
+        while queue:
+            c = queue.pop()
+            res.append(c)
+            for neighbor in graph[c]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        if len(res) != len(indegree): return ""
+        return ''.join(res[::])
+

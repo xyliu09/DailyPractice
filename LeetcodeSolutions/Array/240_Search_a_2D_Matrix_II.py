c4ceea664176e45203e95519d6540e29b72f6b2b
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-12-05 10:26:41 -0500

    240

---
+++ LeetcodeSolutions/Array/240_Search_a_2D_Matrix_II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = len(matrix) - 1
+        col = 0
+        while row >= 0 and col <= len(matrix[0]) - 1:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:
+                return True
+
+        return False

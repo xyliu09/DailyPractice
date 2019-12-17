2a7198bbca73786d9e3950bb89206fe7397663fe
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2019-12-17 08:30:34 -0500

    53_Maximum_Subarray.py

--- LeetcodeSolutions/Dynamic_Programming/53_Maximum_Subarray.py
+++ LeetcodeSolutions/Dynamic_Programming/53_Maximum_Subarray.py
@@ -8,3 +8,19 @@ class Solution(object):
         for i in range(1, len(nums)):
             dp[i] = max(dp[i-1]+nums[i], nums[i])
         return max(dp)
+
+    '''
+    class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxSum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+            maxSum = max(maxSum, nums[i])
+        return maxSum
+    
+    '''

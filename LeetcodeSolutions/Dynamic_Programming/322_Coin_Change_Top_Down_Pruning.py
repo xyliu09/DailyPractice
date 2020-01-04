a7a0639dcc96af14154fadd34a17c2f03acaca02
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-04 11:59:26 -0500

    322

---
+++ LeetcodeSolutions/Dynamic_Programming/322_Coin_Change_Top_Down_Pruning.py
@@ -0,0 +1,21 @@
+#dfs 强剪枝
+class Solution:
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        coins.sort(reverse=True)
+        self.ans = float('inf')
+
+        def dfs(s, amount, count):
+            if amount == 0:
+                self.ans = count
+                return
+            if s == len(coins): return
+            coin = coins[s]
+            for k in range(amount // coin, -1, -1):
+                if count + k > self.ans:
+                    break
+                dfs(s + 1, amount - k * coin, count + k)
+
+        dfs(0, amount, 0)
+        return -1 if self.ans == float('inf') else self.ans

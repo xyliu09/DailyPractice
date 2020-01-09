e4d897e3242ee6354e0ec7fcf18191cfec3600ff
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-08 20:35:22 -0500

    3_Longest_Substring_Without_Repeating_Characters.py

--- LeetcodeSolutions/Two_Pointers/3_Longest_Substring_Without_Repeating_Characters.py
+++ LeetcodeSolutions/Two_Pointers/3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        stringlist = []
-        maxlength = 0
-        table = {}
-        head = 0
-        for x in s:
-            if x in stringlist[head:]:
-                head = table[x]+1
-            stringlist.append(x)
-            table[x] = len(stringlist)-1
-            maxlength = max(maxlength, len(stringlist)-head)
-        return maxlength
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res, i = 0, 0
+        while i < len(s):
+            j = i + 1
+            seen = {s[i]: i}
+            while j < len(s):
+                if s[j] in seen:
+                    break
+                seen[s[j]] = j
+                j += 1
+            res = max(j - i, res)
+            i += 1
+        return res

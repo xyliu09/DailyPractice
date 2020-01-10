1718e942c5cd0f0cf1b021585c2e1c52157c4d13
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-09 21:48:06 -0500

    23_Merge_k_Sorted_Lists.py

--- LeetcodeSolutions/Linkedlist/23_Merge_k_Sorted_Lists.py
+++ LeetcodeSolutions/Linkedlist/23_Merge_k_Sorted_Lists.py
@@ -35,3 +35,34 @@ class Solution(object):
             if node:
                 q.put((node.val, node))
         return head.next
+    '''
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        interval = 1
+        while interval <= len(lists):
+            for i in range(0, len(lists)-interval, interval*2):
+                lists[i] = self.mergeTwoLists(lists[i], lists[i+interval])
+            interval *= 2
+        return lists[0] if len(lists) > 0 else None
+        
+    def mergeTwoLists(self, l1, l2):
+        dummy = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next= l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2  
+        return dummy.next
+        s
+    
+    '''

bdd10133d1919475a26df5381d5d5d385e14d95d
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-11 10:06:54 -0500

    138

--- LeetcodeSolutions/Linkedlist/138_Copy_List_with_Random_Pointer.py
+++ LeetcodeSolutions/Linkedlist/138_Copy_List_with_Random_Pointer.py
@@ -1,34 +1,32 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val, next, random):
-        self.val = val
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
         self.next = next
         self.random = random
 """
-
-
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        if not head: return None
-        p = head
-        while p:
-            nxt = p.next
-            copy = Node(p.val, nxt, None)
-            p.next = copy
-            p = nxt
+        curr = head
+        while curr:
+            node = Node(curr.val, curr.next)
+            curr.next = node
+            curr = curr.next.next
 
-        p = head
-        while p:
-            if p.random:
-                p.next.random = p.random.next
-            p = p.next.next
-        dum = Node(0, None, None)
-        p1, p2 = dum, head
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+
+        dummy = Node(0, None, None)
+        p1, p2 = dummy, head
         while p2:
             p1.next = p2.next
             p1 = p1.next
             p2.next = p2.next.next
             p2 = p2.next
+        return dummy.next
 
-        return dum.next
+        

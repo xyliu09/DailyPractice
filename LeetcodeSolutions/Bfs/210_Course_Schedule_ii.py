c45bb58e7a1abb3fdcb9fbbf18a7d187daf85d19
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-11 21:06:21 -0500

    210_Course_Schedule_ii.py

--- LeetcodeSolutions/Bfs/210_Course_Schedule_ii.py
+++ LeetcodeSolutions/Bfs/210_Course_Schedule_ii.py
@@ -1,27 +1,17 @@
-from collections import defaultdict
-
-
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        graph = defaultdict(list)
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(list)
         indegree = {}
-        topo = []
-        for des, src in prerequisites:
-            graph[src].append(des)
-            indegree[des] = indegree.get(des, 0) + 1
-        stack = [k for k in range(numCourses) if k not in indegree]
-        while stack:
-            node = stack.pop()
-            topo.append(node)
-            if node in graph:
-                for nei in graph[node]:
-                    indegree[nei] -= 1
-                    if indegree[nei] == 0:
-                        stack.append(nei)
-
-        return topo if len(topo) == numCourses else []
+        for x, y in prerequisites:
+            graph[y].append(x)
+            indegree[x] = indegree.get(x, 0) + 1
+        queue = [i for i in range(numCourses) if i not in indegree]
+        ans = []
+        while queue:
+            c = queue.pop(0)
+            ans.append(c)
+            for neighbor in graph[c]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        return ans if len(ans) == numCourses else []

d864d492b396d3c318afaa13012c37835b215612
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-12 16:42:14 -0500

    139_Word_Break_Iterative.py

--- LeetcodeSolutions/Dynamic_Programming/139_Word_Break_Recursive_With_Memo.py
+++ LeetcodeSolutions/Dynamic_Programming/139_Word_Break_Recursive_With_Memo.py
@@ -1,24 +1,13 @@
-class Solution(object):
-    def __init__(self):
-        self.memo = {}
-
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        if s in self.memo:
-            return self.memo[s]
-        if not s or s in wordDict:
-            self.memo[s] = True
-            return True
-
-        for i in range(len(s)):
-            if s[:i + 1] in wordDict and self.wordBreak(s[i + 1:], wordDict):
-                self.memo[s] = True
+from functools import lru_cache
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @lru_cache
+        def dfs(s):
+            if not s or s in wordDict:
                 return True
-        self.memo[s] = False
-        return False
+            for i in range(len(s)):
+                if s[:i + 1] in wordDict and dfs(s[i + 1:]):
+                    return True
+            return False
 
-
+        return dfs(s)

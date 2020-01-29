c5d39114f880fb0eb099c4b05d305a75cda16b2d
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-01-28 23:56:08 -0500

    84

---
+++ DailyPractice/Stack/84_Largest Rectangle in Histogram.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        heights.append(0)
+        stack, res = [-1], 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] -1
+                res = max(res, w * h)
+            stack.append(i)
+        return res

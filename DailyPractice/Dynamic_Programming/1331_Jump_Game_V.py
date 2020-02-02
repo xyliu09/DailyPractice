91646d0122b6d24e02dbafcf5e7642d13adb8785
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2020-02-02 08:28:28 -0500

    1331

--- DailyPractice/Dynamic_Programming/1331_Jump_Game_V.py
+++ DailyPractice/Dynamic_Programming/1331_Jump_Game_V.py
@@ -1,24 +1,21 @@
 class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         from functools import lru_cache
+
         @lru_cache(maxsize=None)
         def dfs(i):
             res = 1
-            for k in range(1, d + 1):
-                j = i + k
-                if 0 <= j < len(arr):
-                    if arr[j] >= arr[i]: break
-                    res = max(res, 1 + dfs(j))
-            for k in range(-1, -1 - d, -1):
-                j = i + k
-                if 0 <= j < len(arr):
-                    if arr[j] >= arr[i]: break
-                    res = max(res, 1 + dfs(j))
+            for j in range(i + 1, min(i + d + 1, len(arr))):
+                if arr[j] >= arr[i]: break
+                res = max(res, 1 + dfs(j))
+
+            for j in range(i - 1, max(i - 1 - d, -1), -1):
+                if arr[j] >= arr[i]: break
+                res = max(res, 1 + dfs(j))
+
             return res
 
         res = 1
         for i in range(len(arr)):
             res = max(res, dfs(i))
         return res
-
-

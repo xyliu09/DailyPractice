aa925ef83624261fd5b4b8ba140de26ba8cd0074
xyliu09 <38929468+xyliu09@users.noreply.github.com>
2021-05-13 21:59:47 -0400

    OOD-1 Design Library Management

--- OOD/DesignParkingLot.py
+++ OOD/DesignParkingLot.py
@@ -2,22 +2,17 @@
 from abc import ABC
 from enum import Enum
 
-
 class VehicleType(Enum):
     CAR, TRUCK, ELECTRIC, VAN, MOTORBIKE = 1, 2, 3, 4, 5
-
 
 class ParkingSpotType(Enum):
     HANDICAPPED, COMPACT, LARGE, MOTORBIKE, ELECTIRC = 1, 2, 3, 4, 5
 
-
 class AccountStatus(Enum):
     ACTIVE, BLOCKED, BANNED, COMPROMISED, ARCHIVED, UNKNOWN = 1, 2, 3, 4, 5, 6
 
-
 class ParkingTicketStatus(Enum):
     ACTIVE, PAID, LOST = 1, 2, 3
-
 
 class Address:
     def __init__(self, street, city, state, zip_code, country):
@@ -27,14 +22,12 @@ class Address:
         self.__zip_code = zip_code
         self.__country = country
 
-
 class Person():
     def __init__(self, name, address, email, phone):
         self.__name = name
         self.__address = address
         self.__email = email
         self.__phone = phone
-
 
 # Account, Admin, and ParkingAttendant: These classes represent various people that interact with our system
 class Account:
